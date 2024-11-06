e1c07d2f98ddccf6e85dbaf2930ef59667cc099d
Francesco <alemannof43@gmail.com>
2024-11-06 19:12:43 +0100

    aggiunto file JavaScript che permette di far funzionare le frecce

---
+++ MAIN/script.js
@@ -0,0 +1,31 @@
+// script.js
+const moveLeftButton = document.getElementById("move-left");
+const moveRightButton = document.getElementById("move-right");
+const cardCaroselloWrapper = document.querySelector(".card");
+
+function moveCards(distance) {
+    cardCaroselloWrapper.scrollBy({
+        left: distance,
+        behavior: 'smooth' 
+    });
+}
+
+
+moveLeftButton.addEventListener("click", function() {
+    moveCards(-320); 
+});
+
+moveRightButton.addEventListener("click", function() {
+    moveCards(320); 
+});
+
+
+cardCaroselloWrapper.addEventListener("scroll", function() {
+    
+    const scrollLeft = cardCaroselloWrapper.scrollLeft;
+    const maxScrollLeft = cardCaroselloWrapper.scrollWidth - cardCaroselloWrapper.clientWidth;
+
+    
+    moveLeftButton.style.opacity = scrollLeft > 0 ? 1 : 0.5; 
+    moveRightButton.style.opacity = scrollLeft < maxScrollLeft ? 1 : 0.5; 
+});
