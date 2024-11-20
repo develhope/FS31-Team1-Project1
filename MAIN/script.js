2d30b47dabca7d0e6c91589c5717f41096df5f66
gianlorenzo mungiovino <gianlorenzo.mungiovino@gmail.com>
2024-11-20 20:04:21 +0100

    fixati bug dissolvenza navbar e lente

--- MAIN/script.js
+++ MAIN/script.js
@@ -1,62 +1,73 @@
-
 //Sezione della lente
-document.addEventListener('DOMContentLoaded', function() {
-    const lente = document.querySelector('.lente');
-    const inputContenitoreCerca = document.querySelector('.input-contenitore-cerca');
-    const iconaX = document.querySelector('.icona-x');
-    const overlay = document.querySelector('.overlay');
-   
-  
-    // Nasconde inizialmente l'input
-    inputContenitoreCerca.style.display = 'none';
-    overlay.style.display = 'none';
-  
-    // Quando si clicca sulla lente
-    lente.addEventListener('click', function() {
-      lente.style.display = 'none';
-      inputContenitoreCerca.style.display = 'flex';
-      overlay.style.display = 'block'; 
-      document.body.classList.add('no-scroll');
-    });
-  
-    // Quando si clicca sulla X 
-    iconaX.addEventListener('click', function() {
-      inputContenitoreCerca.style.display = 'none';
-      lente.style.display = 'block';
-      overlay.style.display = 'none'; 
-      document.body.classList.remove('no-scroll');
+document.addEventListener("DOMContentLoaded", function () {
+  const lente = document.querySelector(".lente");
+  const inputContenitoreCerca = document.querySelector(
+    ".input-contenitore-cerca"
+  );
+  // const inputLente = document.querySelector("lenteuno icona-cerca");
+  const iconaX = document.querySelector(".icona-x");
+  const overlay = document.querySelector(".overlay");
 
-    });
+  // Nasconde inizialmente l'input
+  inputContenitoreCerca.style.display = "none";
+  overlay.style.display = "none";
+
+  // Quando si clicca sulla lente
+  lente.addEventListener("click", function () {
+    lente.style.display = "none";
+    inputContenitoreCerca.style.display = "flex";
+    overlay.style.display = "block";
+    document.body.classList.add("no-scroll");
   });
+
+  // Quando si clicca sulla X
+  iconaX.addEventListener("click", function () {
+    inputContenitoreCerca.style.display = "none";
+    lente.style.display = "block";
+    overlay.style.display = "none";
+    document.body.classList.remove("no-scroll");
+  });
+
+  // // Quando si clicca sul campo di input, preveniamo lo scroll verso l'alto
+  // inputContenitoreCerca.addEventListener("focus", function (event) {
+  //   event.preventDefault(); // Preveniamo il comportamento predefinito
+  //   // Scrolla in modo controllato per non causare il movimento verso l'alto
+  //   inputContenitoreCerca.scrollIntoView({
+  //     behavior: "smooth",
+  //     block: "center",
+  //   });
+  // });
+});
 
 //fINE SEZIONE LENTE
 
 //Inizio sezione dissolvenza
 
 let lastScrollPosition = 0;
-const navbar = document.querySelector('.navbar');
-const triggerHeight = 126; 
+const navbar = document.querySelector(".navbar-bottom");
+console.log(navbar);
 
-window.addEventListener('scroll', () => {
-    const currentScrollPosition = window.scrollY;
+const triggerHeight = 1;
+// const triggerHeight = 126;
 
-    if (currentScrollPosition < triggerHeight) {
-        // Rimuovi le classi fixed e hidden se siamo sopra il triggerHeight
-        navbar.classList.remove('fixed','hidden');
-    } 
-    
-    else if (currentScrollPosition > lastScrollPosition) {
-        // Nascondi la navbar quando si scorre verso il basso
-        navbar.classList.add( 'hidden');
-        //navbar.classList.remove('fixed');
-    } else {
-        // Mostra la navbar quando si scorre verso l'alto
-        navbar.classList.remove('hidden');
-        navbar.classList.add('fixed');
-        //Portare il correnscroll = 0 e disattivare il fixed solo in quel caso
-    }
+window.addEventListener("scroll", () => {
+  const currentScrollPosition = window.scrollY;
 
-    lastScrollPosition = currentScrollPosition; // Aggiorna la posizione di scorrimento
+  if (currentScrollPosition < triggerHeight) {
+    // Rimuovi le classi fixed e hidden se siamo sopra il triggerHeight
+    navbar.classList.remove("fixed", "hidden");
+  } else if (currentScrollPosition > lastScrollPosition) {
+    // Nascondi la navbar quando si scorre verso il basso
+    navbar.classList.add("hidden");
+    //navbar.classList.remove('fixed');
+  } else {
+    // Mostra la navbar quando si scorre verso l'alto
+    navbar.classList.remove("hidden");
+    navbar.classList.add("fixed");
+    //Portare il correnscroll = 0 e disattivare il fixed solo in quel caso
+  }
+
+  lastScrollPosition = currentScrollPosition; // Aggiorna la posizione di scorrimento
 });
 
 //Fine sezione dissolvenza
